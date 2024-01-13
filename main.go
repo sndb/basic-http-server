70395016607836c3f0455f9a2dc2b13653ee41a7
sndb <sndb@sndb.xyz>
2024-01-13 11:17:22 +0300

    s/Printf/Print

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.RequestURI)
+		log.Print(r.RemoteAddr, " ", r.Proto, " ", r.Method, " ", r.RequestURI)
 		next.ServeHTTP(w, r)
 	})
 }
